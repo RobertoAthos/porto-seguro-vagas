0ffae3bc20abf6954c75dff230b23ac910a01e11
Roberto Athos <robertoathos@bricker.vc>
2024-11-15 10:13:03 -0300

    config: initial configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,14 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        palette: {
+          dark: "#1C1E1B",       // 01 - Dark
+          charcoal: "#2F352B",   // 02 - Charcoal
+          lime: "#7DEF70",       // 03 - Lime
+          mint: "#CBF1B7",       // 04 - Mint
+          ivory: "#F3F7EC",      // 05 - Ivory
+          white: "#F9FAF8",      // 06 - White
+        },
       },
     },
   },
