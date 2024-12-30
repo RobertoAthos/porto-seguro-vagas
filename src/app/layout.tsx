4878e4ff4f18e4dafa4631c6319aedf9736941ad
Roberto Athos <robertoathos22@gmail.com>
2024-12-30 10:42:33 -0300

    update: add open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,28 @@ export const metadata: Metadata = {
 	description:
 		"Vagas de emprego em Porto Seguro, Arraial D´Ajuda e Trancoso. Conecte-se às Melhores Oportunidades da Região!",
 	keywords: seoKeywords,
+	openGraph: {
+		title:
+			"Porto Seguro Vagas - Encontre vagas de emprego em Porto Seguro e região",
+		description:
+			"Vagas de emprego em Porto Seguro, Arraial D´Ajuda e Trancoso. Conecte-se às Melhores Oportunidades da Região!",
+		type: "website",
+		locale: "pt_BR",
+		url: "https://portosegurovagas.com",
+		siteName: "Porto Seguro Vagas",
+		images: [
+			{
+				url: "https://portosegurovagas.com/og-image.png",
+				width: 1200,
+				height: 630,
+				alt: "Vagas de emprego em Porto Seguro",
+			},
+		],
+	},
+	metadataBase: new URL("https://portosegurovagas.com/"),
+	twitter: {
+		card: "summary_large_image",
+	},
 };
 
 const dmsans = DM_Sans({
